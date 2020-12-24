a9d8520666c5e0027afe0a7b6d69b462d54c8e0c
kanywst <niwatakuma@icloud.com>
2020-12-25 00:07:17 +0900

    solved number-of-steps-to-reduce-a-number-to-zero

--- number-of-steps-to-reduce-a-number-to-zero/solve.py
+++ number-of-steps-to-reduce-a-number-to-zero/solve.py
@@ -1,13 +1,14 @@
 def numberOfSteps(num):
     cnt = 0
     while True:
-        if num%2==0:
-            num=num//2
-        else:
-            num-=1
-        cnt+=1
         if num==0:
             break
+        elif num%2==0:
+            num=num//2
+            cnt+=1
+        else:
+            num-=1
+            cnt+=1
     return cnt
 
 def main():
