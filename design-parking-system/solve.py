73b68ecd762be1431f56492c929f53fecf80d2e2
kanywst <niwatakuma@icloud.com>
2021-01-30 11:11:32 +0900

    solved design-parking-system

--- design-parking-system/solve.py
+++ design-parking-system/solve.py
@@ -1,9 +1,7 @@
-def __init__(big,medium,small):
+def __init__(self,big,medium,small):
+    self.slots = {1: big, 2: medium, 3: small}
 
-def addCar(carType):
-
-
-def main():
-
-if __name__ == "__main__":
-    main()
+def addCar(self,carType):
+    if self.slots[carType] == 0: return False
+    self.slots[carType] -= 1
+    return True
