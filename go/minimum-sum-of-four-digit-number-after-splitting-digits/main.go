83a653a9188a17fdda4231d28c4c624aa280861d
kanywst <niwatakuma@icloud.com>
2023-02-24 21:05:08 +0900

    ✅2160. Minimum Sum of Four Digit Number After Splitting Digits

--- go/minimum-sum-of-four-digit-number-after-splitting-digits/main.go
+++ go/minimum-sum-of-four-digit-number-after-splitting-digits/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
+	"strings"
 )
-
-func minimumSum(num int) int {
-	s := strconv.Itoa(num)
-
-}
 
 func main() {
 	num := 2932
 	fmt.Println(minimumSum(num))
+}
+
+func minimumSum(num int) int {
+	s := strings.Split(strconv.Itoa(num), "")
+	sort.Strings(s)
+	tmp_1, _ := strconv.Atoi(s[0] + s[2])
+	tmp_2, _ := strconv.Atoi(s[1] + s[3])
+	return tmp_1 + tmp_2
 }
