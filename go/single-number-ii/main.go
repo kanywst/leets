a6b117bd9d5e6e5af5bd14a90a7f73b0fbf46d37
kanywst <niwatakuma@icloud.com>
2023-02-28 21:20:31 +0900

    ✅137. Single Number II

---
+++ go/single-number-ii/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{0, 1, 0, 1, 0, 1, 99}
+	fmt.Println(singleNumber(nums))
+}
+func singleNumber(nums []int) (ans int) {
+	m := make(map[int]int)
+	for _, i := range nums {
+		m[i] += 1
+	}
+	for k, v := range m {
+		if v == 1 {
+			ans = k
+			break
+		}
+	}
+	return
+}
